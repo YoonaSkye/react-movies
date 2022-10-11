1869cea514f2f71350132e91fa246e2fef429d4b
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-10-11 14:44:20 +0800

    Make movie list and movie card

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "",
+  apiKey: "enter your api key",
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
