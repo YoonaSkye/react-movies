7e72cbf16da0b8cd5d76193edd2426f74db8be0f
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-10-13 23:30:08 +0800

    private APi key

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "",
+  apiKey: process.env.REACT_APP_MOVIE_API,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
